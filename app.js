10916380e9c0af7784fa10db486c496e8c0570a7
masiyematome <matomemasiye7@gmail.com>
2020-12-04 08:24:29 +0200

    Added the check pseudo element on the to do

--- app.js
+++ app.js
@@ -3,43 +3,55 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const todoFooter = document.querySelector(".todo-footer");
 
 //Event Listeners
 
-todoButton.addEventListener("click",addTask);
-todoList.addEventListener("click",deleteOrMark);
+todoButton.addEventListener("click", addTask);
+todoList.addEventListener("click", deleteOrMark);
 
 //Functions
 
-function addTask(){
+function addTask() {
 
-    const todoItem = document.createElement("div");
-    todoItem.classList.add("todo-item");
+    if (todoInput.value == "") {
+        alert("You can not add an empty task");
+    }
 
-    const todoText = document.createElement("li");
-    todoText.classList.add("todo-text");
-    todoText.innerText = "I am a text so please respect me";
-    todoItem.appendChild(todoText);
+    else {
+        const todoItem = document.createElement("div");
+        todoItem.classList.add("todo-item");
 
-    const trashButton = document.createElement("button");
-    trashButton.classList.add("trash-button");
-    trashButton.innerHTML = '<i class = "fa fa-trash"></i>';
-    todoItem.appendChild(trashButton);
-    
-    todoList.appendChild(todoItem);
+        const todoText = document.createElement("li");
+        todoText.classList.add("todo-text");
+        todoText.innerText = todoInput.value;
+        todoItem.appendChild(todoText);
+
+        const trashButton = document.createElement("button");
+        trashButton.classList.add("trash-button");
+        trashButton.innerHTML = '<i class = "fa fa-trash"></i>';
+        todoItem.appendChild(trashButton);
+
+        todoList.appendChild(todoItem);
+        todoInput.value = "";
+
+        todoFooter.style.display = "block";
+
+    }
 
 }
 
-function deleteOrMark(ev){
+function deleteOrMark(ev) {
     const clicked = ev.target;
 
-    if(clicked.classList[0] == "trash-button"){
+    if (clicked.classList[0] == "trash-button") {
         const todo = clicked.parentElement;
         todo.remove();
     }
 
-    else{
-        console.log("I will be marked");
+    else {
+        const todo = clicked.parentElement;
+        todo.classList.toggle("checked");
     }
 
 }
